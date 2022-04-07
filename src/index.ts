3df04a25b77b9c8b4f5df2f7381c639124ce2b3c
Dani <danizt96@gmail.com>
2022-04-07 21:23:07 +0200

    install and config eslint and prettier

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express from 'express'
+import express from "express"
 
 const app = express()
 
@@ -6,9 +6,9 @@ app.use(express.json())
 
 const PORT = 3000
 
-app.get('/ping', (_req, res) => {
-  console.log('someone pinged here!!') 
-  res.send('pong')
+app.get("/ping", (_req, res) => {
+  console.log("someone pinged here!!")
+  res.send("pong")
 })
 
 app.listen(PORT, () => {
